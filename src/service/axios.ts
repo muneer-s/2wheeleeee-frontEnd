5c301f8f08c0522a60b88795c5ed8ec41e7022fa
Muneer <muneerskoppam1107@gmail.com>
2025-03-05 14:44:55 +0530

    code front end

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const Api: AxiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_B_URI}/api`,
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
     withCredentials: true
 })
 
